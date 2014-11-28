02c9234070eba881e9ddd6167d07cd0531b0e477
Ted Goldstein <tedgoldstein@gmail.com>
2014-11-28 12:36:21 -0800

    Streamlined Gateway2

--- Gateway.coffee
+++ Gateway.coffee
@@ -71,9 +71,12 @@ splitHostPort = (s) ->
 class Proxy
   constructor: () ->
     @internal_domains = {}
-    @http_proxy = httpProxy.createProxyServer({})
-    @http_proxy.on 'upgrade', (req, socket, head) =>
-          @http_proxy.ws req, socket, head
+    @http_proxy = httpProxy.createProxyServer({ws:true})
+    @http_proxy.on 'upgrade', (req, socket) =>
+          console.log "UPGRADE"
+          process.exit 100
+          @http_proxy.ws req, socket, { target: "http://localhost:10003"}
+
     @http_proxy.on 'error', (e) =>
           if e.code != "ECONNRESET"
               console.log "error", e
@@ -90,7 +93,7 @@ class Proxy
         if menuItem == null
             menuItem = ca.route
         console.log "menuItem", menuItem, typeof(menuItem)
-        menuItem = menuItem.replace(" ", "&nbsp;")
+        menuItem = menuItem.replace /\ /g, "&nbsp;"
         link = "<a class='MedBookLink' href='"+ca.route+"'>" + menuItem + "</a>"
         if ca.menuItem
             if ca.menuPosition != undefined
@@ -137,70 +140,3 @@ console.log "watching",  args[0]
 fs.watchFile args[0], run
 fs.watchFile "menu.html", readMenu
 
-style="<style> \
-body { \
-  font-family: 'Lucida Grande', 'Helvetica Neue', Helvetica, Arial, sans-serif; \
-  padding: 20px 50px 150px; \
-  font-size: 13px; \
-  text-align: center; \
-  background: #E3CAA1; \
- \
- \
-ul { \
-  text-align: left; \
-  display: inline; \
-  margin: 0; \
-  padding: 15px 4px 17px 0; \
-  list-style: none; \
-  -webkit-box-shadow: 0 0 5px rgba(0, 0, 0, 0.15); \
-  -moz-box-shadow: 0 0 5px rgba(0, 0, 0, 0.15); \
-  box-shadow: 0 0 5px rgba(0, 0, 0, 0.15); \
-} \
-ul li { \
-  font: bold 12px/18px sans-serif; \
-  display: inline-block; \
-  margin-right: -4px; \
-  position: relative; \
-  padding: 15px 20px; \
-  background: #fff; \
-  cursor: pointer; \
-  -webkit-transition: all 0.2s; \
-  -moz-transition: all 0.2s; \
-  -ms-transition: all 0.2s; \
-  -o-transition: all 0.2s; \
-  transition: all 0.2s; \
-} \
-ul li:hover { \
-  background: #555; \
-  color: #fff; \
-} \
-ul li ul { \
-  padding: 0; \
-  position: absolute; \
-  top: 48px; \
-  left: 0; \
-  width: 150px; \
-  -webkit-box-shadow: none; \
-  -moz-box-shadow: none; \
-  box-shadow: none; \
-  display: none; \
-  opacity: 0; \
-  visibility: hidden; \
-  -webkit-transiton: opacity 0.2s; \
-  -moz-transition: opacity 0.2s; \
-  -ms-transition: opacity 0.2s; \
-  -o-transition: opacity 0.2s; \
-  -transition: opacity 0.2s; \
-} \
-ul li ul li {  \
-  background: #555;  \
-  display: block;  \
-  color: #fff; \
-  text-shadow: 0 -1px 0 #000; \
-} \
-ul li ul li:hover { background: #666; } \
-ul li:hover ul { \
-  display: block; \
-  opacity: 1; \
-  visibility: visible; \
-}</style>"
---
+++ Gateway2.js
@@ -0,0 +1,159 @@
+var args = process.argv.slice(2);
+if (args.length < 1) {
+  console.log("Please specify configuration file");
+  process.exit(1);
+}
+
+var Cookies = require('cookies')
+var toml = require('toml')
+var fs = require('fs')
+
+
+var server = null;
+
+menuFile = null;
+
+getPort = function(req) {
+    var a = req.url.split("/");
+    if (a.length > 2) {
+        var p = routes["/" + a[1]];
+        if (p)
+            return p;
+    }
+    return final;
+}
+
+readMenu = function() {
+  menuFile = fs.readFileSync("menu.html");
+  return console.log("menu.html read", menuFile.length);
+};
+
+readMenu();
+
+run = function() {
+  if (server) {
+    server.close();
+  }
+
+  configApp(args[0]);
+  console.log("loading", args[0]);
+
+  server = require('http').createServer(function(req, res) {
+
+    if (req.url == "/menu")
+        return serveMenu(req, res);
+
+    var port = getPort(req);
+    console.log("web", req.url, port)
+    proxy.web(req, res, {
+      target: "http://localhost:"+port,
+    },function(e){
+      log_error(e,req);
+    });
+  })
+
+  var httpProxy = require('http-proxy')
+  var proxy = httpProxy.createProxy({ ws : true });
+
+  server.on('upgrade',function(req,res){
+    var port = getPort(req);
+    console.log("ws upgrade", req.url, port);
+    proxy.ws(req, res, {
+      target: "http://localhost:" + port,
+    },function(e){
+      log_error(e, req);
+    });
+  })
+   
+  server.listen(10000)
+};
+
+splitHostPort = function(s) {
+  var parts, ret;
+  parts = s.split(':', 2);
+  ret = {
+    host: parts[0]
+  };
+  if (parts.length > 1) {
+    ret.port = parseInt(parts[1], 10);
+  } else {
+    ret.port = 80;
+  }
+  return ret;
+};
+
+var configuration = null;
+var routes = null;
+var auth = null;
+var final = null;
+var config = null;
+
+configApp = function(path) {
+  try {
+    config = toml.parse(fs.readFileSync(path));
+  } catch (_error) {
+    console.log("Failed to parse configuration file " + path);
+    return process.exit(1);
+  }
+
+  var appName, ca, link, menu, menuItem, _ref;
+  menu = [];
+  routes = {};
+  auth = {};
+  console.log(config.final);
+  final = config.final.port
+
+  _ref = config.apps;
+  for (appName in _ref) {
+    var ca = _ref[appName];
+    routes[ca.route] = ca.port;
+    auth[ca.route] = ca.auth;
+  }
+
+  return console.log("configApp menu", menu);
+};
+
+serveMenu = function(req, res) {
+  var menu = [];
+  _ref = config.apps;
+  for (appName in _ref) {
+    var ca = _ref[appName];
+    menuItem = String(ca.menuItem);
+    if (menuItem === null) {
+      menuItem = ca.route;
+    }
+    console.log("menuItem", menuItem, typeof menuItem);
+    menuItem = menuItem.replace(/\ /g, "&nbsp;");
+    link = "<a class='MedBookLink' href='" + ca.route + "'>" + menuItem + "</a>";
+    if (ca.menuItem) {
+      if (ca.menuPosition !== void 0) {
+        menu.splice(ca.menuPosition, 0, link);
+      } else {
+        menu.push(link);
+      }
+    }
+  }
+
+  var text = String(menuFile).replace("LIST", (menu.map(function(a) {
+    return "<li>" + a + "</li><br/>";
+  })).join(''));
+  res.writeHead(200);
+  res.write(text, "binary");
+  return res.end();
+};
+
+
+run();
+
+console.log("watching", args[0]);
+
+fs.watchFile(args[0], run);
+fs.watchFile("menu.html", readMenu);
+
+function log_error(e,req){
+  if(e){
+    console.error(e.message);
+    console.log(req.headers.host,'-->');
+    console.log('-----');
+  }
+}
