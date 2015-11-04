72ba74ca4bb5c63e2f2a0d12e914333a7be147bf
Abigail Watson <awatson1978@gmail.com>
2015-11-04 14:31:30 -0800

    updated to clinical:router

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 
 Package.describe({
   name: 'clinical:collaborations',
-  version: '2.1.5',
+  version: '2.1.6',
   summary: 'Collaboration based security architecture (similar to Roles and Friends)',
   git: 'https://github.com/clinical-meteor/clinical-collaborations',
   documentation: 'README.md'
