e4de31969c29697c30bc6348009c8155c0fa1438
Robert Baertsch <baertsch@soe.ucsc.edu>
2015-12-07 17:05:08 -0800

    update version

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'medbook:primary-collections',
-  version: '0.0.6',
+  version: '0.0.7',
   // Brief, one-line summary of the package.
   summary: 'Primary collections and schemas for MedBook',
   // URL to the Git repository containing the source code for this package.
