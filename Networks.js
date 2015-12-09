d7d51cd454953c8a1d3152c57ab2ecfcd3ef1edf
mokolodi1 <mokolodi1@gmail.com>
2015-12-09 15:34:40 -0800

    added date_created to Networks

--- Networks.js
+++ Networks.js
@@ -7,6 +7,19 @@ Networks.attachSchema(new SimpleSchema({
   collaborations: { type: [String] },
   name: { type: String },
   version: { type: Number },
+  date_created: {
+    type: Date,
+    // https://github.com/aldeed/meteor-collection2#autovalue
+    autoValue: function() {
+      if (this.isInsert) {
+        return new Date();
+      } else if (this.isUpsert) {
+        return { $setOnInsert: new Date() };
+      } else {
+        this.unset();  // Prevent user from supplying their own value
+      }
+    },
+  },
 
   type: {
     type: String,
