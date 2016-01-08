a38b913c0da13afff65765ba792c9c85f58a3a7c
mokolodi1 <mokolodi1@gmail.com>
2016-01-08 16:25:10 -0500

    bump

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: "medbook:primary-collections",
-  version: "0.0.12",
+  version: "0.0.13",
   // Brief, one-line summary of the package.
   summary: "Primary collections and schemas for MedBook",
   // URL to the Git repository containing the source code for this package.
