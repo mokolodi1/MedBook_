4090e6ff314cd2d4cf63983e52292fad106957b1
mokolodi1 <mokolodi1@gmail.com>
2016-01-08 10:02:38 -0500

    bump

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'medbook:wrangler-collections',
-  version: '0.0.17',
+  version: '0.0.18',
   // Brief, one-line summary of the package.
   summary: 'Collections and such relating to Wrangler',
   // URL to the Git repository containing the source code for this package.
