7adcbb64111b6127b611972e36a2fbe137f6f2f2
mokolodi1 <mokolodi1@gmail.com>
2016-01-26 18:10:09 -0800

    finalized Signatures schema

--- Signatures.js
+++ Signatures.js
@@ -1,10 +1,10 @@
 Signatures = new Meteor.Collection("signatures");
 
-var geneValuePair = new SimpleSchema({
-  gene_label: { type: String },
+var featureValuePair = new SimpleSchema({
+  feature_label: { type: String },
   value: { type: Number, decimal: true },
   p_value: { type: Number, decimal: true, optional: true },
-  probability: { type: Number, decimal: true, optional: true }
+  false_discovery_rate: { type: Number, decimal: true, optional: true }
 });
 
 var sampleSchema = new SimpleSchema({
@@ -14,6 +14,8 @@ var sampleSchema = new SimpleSchema({
 });
 
 // calculate allowedValues for training_normalization
+// NOTE/TODO: we can't just pull this from the field in Wrangler because
+// quantile_counts_log needs to be an option (or does it?)
 var schema = GeneExpression.simpleSchema().schema();
 var normalizationKeys = _.filter(Object.keys(schema),
     function (value) {
@@ -41,44 +43,63 @@ var training_normalization = {
 
 Signatures.attachSchema(new SimpleSchema({
   user_id: { type: String },
-  collaborations: { type: String },
+  collaborations: { type: [String] },
 
   // label, version uniquely identify signature
   signature_label: { type: String }, // ex. "ABL1_kinase_viper_v4"
   signature_version: { type: Number, min: 1 },
-  signature_type: {
+
+  description: { type: String }, // "KEAP1 non-silent mutation"
+
+  algorithm: {
     type: String,
     allowedValues: [
-      "tf",
-      "kinase",
-      "drug",
-      "mutation",
-      "other",
-    ],
-  },
-  signature_algorithm: {
-    type: String,
-    allowedValues: [
+      "limma",
       "viper"
     ],
   },
 
-  description: { type: String }, // "KEAP1 non-silent mutation"
-  gene_label: { type: String, optional: true },
+  // NOTE: dense/sparse weights stored as seperate signatures
+  features: { type: [featureValuePair], optional: true },
+  features_type: { // the type for features.$.feature_label
+    type: String,
+    allowedValues: [
+      "genes",
+      "isoforms",
+    ],
+  },
 
-  // TODO: upper/lower ==> a/b ?
-  upper_name: { type: String, optional: true },
-  upper_threshold: { type: String, optional: true },
-  upper_training_samples: { type: [sampleSchema], optional: true },
+  // // this is for linking to more information about a gene or drug
+  // link_label: { type: String, optional: true, }, // previously gene_label
+  // link_type: {
+  //   type: String,
+  //   optional: true,
+  //   allowedValues: [
+  //     "gene"
+  //   ]
+  // },
+  //
+  // // for use in gene_expression type
+  // training_normalization: training_normalization,
+  //
+  // upper_name: { type: String, optional: true },
+  // // TODO: does limma give thresholds
+  // upper_threshold: { type: String, optional: true },
+  // upper_training_samples: { type: [sampleSchema], optional: true },
+  //
+  // lower_name: { type: String, optional: true },
+  // lower_threshold: { type: String, optional: true },
+  // lower_training_samples: { type: [sampleSchema], optional: true },
 
-  lower_name: { type: String, optional: true },
-  lower_threshold: { type: String, optional: true },
-  lower_training_samples: { type: [sampleSchema], optional: true },
-
-  training_normalization: training_normalization,
-
-  // NOTE: dense/sparse weights stored as seperate signatures?
-  gene_weights: { type: [geneValuePair], optional: true },
-  // dense_weights: { type: [geneValuePair], optional: true },
-  // sparse_weights: { type: [geneValuePair], optional: true },
+  // // refers to the collection
+  // type: {
+  //   type: String,
+  //   allowedValues: [
+  //     "gene_expression",
+  //     // "drug",
+  //     // "mutation",
+  //     // "other",
+  //     "differential" // ??
+  //   ],
+  // },
 }));
