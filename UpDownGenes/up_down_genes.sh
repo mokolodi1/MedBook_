ca52e56ed1661399ec778126a8843cc1801e2bec
mokolodi1 <mokolodi1@gmail.com>
2016-02-10 18:34:05 -0800

    renamed compendium analysis

--- UpDownGenes/up_down_genes.sh
+++ UpDownGenes/up_down_genes.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
 
-# arg 1: matrix file
-# arg 2: default 1.5
-/usr/bin/Rscript outlier.R mRNA.NBL.POG.pancan.combat.5.tab 2
-
 # ./POG079.txt: gene expression for that one sample
-join -1 1 ./highthreshold.txt ./lowthreshold.txt | join -1 1 - ./POG079.txt > temp
+join -1 1 ./highthreshold.txt ./lowthreshold.txt | join -1 1 - ./test_sample_gene_expression.txt > temp
 awk '($3>$4){print $1, $4, "down_outlier"}' temp > expression_down_outliers.tsv
 awk '($2<$4){print $1, $4, "up_outlier"}' temp > expression_up_outliers.tsv
