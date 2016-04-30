010ef89cd34b2ad044c054b493f47717da6aa4e3
Teo Fleming <mokolodi1@gmail.com>
2016-04-29 18:52:33 -0700

    changed RunLimmaGSEA args

--- Jobs.js
+++ Jobs.js
@@ -25,10 +25,13 @@ var jobSchemas = {
   },
   RunLimmaGSEA: {
     args: new SimpleSchema({
-      sample_group_id_a: { type: String },
-      sample_group_id_b: { type: String },
+      sample_group_a_id: { type: String },
+      sample_group_b_id: { type: String },
       limma_top_genes_count: { type: Number, min: 1 },
       gene_set_collection_id: { type: String },
+      sample_group_a_name: { type: String },
+      sample_group_b_name: { type: String },
+      gene_set_collection_name: { type: String },
     }),
     output: null,
   },
