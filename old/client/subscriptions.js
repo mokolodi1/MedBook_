846d01cd5f150f7dd2db1ff500f253e84bf7f03e
Teo Fleming <mokolodi1@gmail.com>
2016-05-03 17:52:02 -0700

    delete old

--- old/client/subscriptions.js
+++
@@ -1,5 +0,0 @@
-// We need this so that we get the extra fields in the user objects
-Tracker.autorun(function () {
-  // include Meteor.userId() so that it is reactive on the client
-  Meteor.subscribe("/collaborations/user", Meteor.userId());
-});
--- old/server/accounts.js
+++
@@ -1,6 +0,0 @@
-// Accounts.onLogin(
-//   function(args) {
-//     //env.process.DEBUG && console.log("onLogin", args.user.username, Date.now());
-//     //refreshUserProfileCollaborations(args.user);
-//   }
-// );
