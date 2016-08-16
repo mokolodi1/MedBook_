8116c891c5e4abb18e462a81499efc2fe39e1cdd
e-t-k <e-t-k@users.noreply.github.com>
2016-08-16 11:30:16 -0700

    Update rectangular_file_to_json.py
    schema for gene_expression (now known as genomic_expression) has changed, need to update this file to match.

--- importers/rectangular_file_to_json.py
+++ importers/rectangular_file_to_json.py
@@ -31,15 +31,15 @@ def main():
 
                     geneExpressionDoc["_id"] = str(ObjectId())
                     geneExpressionDoc["data_set_id"] = dataSetId
-                    geneExpressionDoc["gene_label"] = cells[0]
-                    geneExpressionDoc["rsem_quan_log2"] = [float(num) for num in cells[1:]]
+                    geneExpressionDoc["feature_label"] = cells[0]
+                    geneExpressionDoc["values"] = [float(num) for num in cells[1:]]
 
                     print(str(geneExpressionDoc))
 
         # magical incantation to import into mongodb
         # batchSize makes sure each bulk operation is smaller than 16mb
         sys.stderr.write("Use this command to imoprt into mongo:\n")
-        sys.stderr.write("mongoimport --db MedBook --collection gene_expression --file OUTPUTFILE.mongoexport --batchSize 50\n")
+        sys.stderr.write("mongoimport --db MedBook --collection genomic_expression --file OUTPUTFILE.mongoexport --batchSize 50\n")
 
         sys.stderr.write("\nUse this command to get the sample labels:\n")
         sys.stderr.write("./get_data_set_sample_labels data.tab | pbcopy\n")
