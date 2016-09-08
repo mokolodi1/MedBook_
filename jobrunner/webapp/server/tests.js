0896778fbab5b4a5c170a95cd1d0897a72721e04
ETK <e-t-k@users.noreply.github.com>
2016-09-08 11:57:13 -0700

    [untested] run jobrunner tests in travis?
    Let's see if this spins up & runs some tests...

---
+++ jobrunner/webapp/server/tests.js
@@ -0,0 +1,40 @@
+var assert = require('assert');
+import "./startup.js";
+
+// TODO create a test data set with running jobs so we can ensure
+// that startup clears them out
+
+// TODO
+describe('running a job', function(){
+  // add a job
+  // see if it runs
+  let newjob = {status:"waiting", name:"TestJob"};
+  // Jobs.insert(newjob);
+});
+
+describe('no running jobs or wrangling datasets on startup',function(){
+  it('has no running jobs', function(){
+    let runjob = Jobs.findOne({status:"running"});
+    assert.isUndefined(runjob);
+  });
+  it('has no wrangling datasets',function(){
+    let ds = DataSets.findOne({currently_wrangling:true});
+    assert.isUndefined(ds);
+  });
+});
+
+describe('synched cron', function(){
+  it('is scheduled to start another job', function(){
+    let foo = SyncedCron.nextScheduledAtDate("start-next-job");
+    console.log(foo);
+    assert.typeOf(foo, 'Date', "start next job at some date.");
+  });
+});
+
+describe('my first test', function(){
+  it('doesnt do much', function(){
+    console.log("hello world");
+    assert.equal(2,2);
+  });
+});
+
