c55ba094e97671190ec9d668b127c81f6f83e515
Teo Fleming <mokolodi1@gmail.com>
2016-11-21 11:19:20 -0800

    removed old crud from job, fixed settings

--- job-runner/webapp/server/classes/UpdateCbioData.js
+++ job-runner/webapp/server/classes/UpdateCbioData.js
@@ -1,9 +1,6 @@
 function UpdateCbioData(job_id) {
-  console.log('run UpdateCbioData', this, 'job', job_id)
+  console.log('run UpdateCbioData', this, 'job', job_id);
   Job.call(this, job_id);
-  // UpdateCbioData
-  // args
-  //  sample_group_id - samples with expression and clinical data for upload into cbio
 }
 UpdateCbioData.prototype = Object.create(Job.prototype);
 UpdateCbioData.prototype.constructor = UpdateCbioData;
@@ -13,7 +10,6 @@ UpdateCbioData.prototype.run = function () {
   // Use temporary folders at /tmp/UpdateCbioData[job_id]
 
   var workDir = "/tmp/" + "UpdateCbioData_" + this.job._id;
-  console.log('workdir',workDir)
 
   try {
     fs.mkdirSync(workDir);
@@ -25,71 +21,28 @@ UpdateCbioData.prototype.run = function () {
 
   console.log("workDir: ", workDir);
 
-  // look up datasetName using sample_group
-  var group = SampleGroups.findOne(this.job.args.sample_group_id);
-  var dataSetHash = {};
-  var dataSetName = "";
-  _.each(group.data_sets, function (dataSet) {
-    // check if we've seen this data set already
-    var seenAlready = dataSetHash[dataSet.data_set_id];
-    if (!seenAlready) {
-      // if we haven't, set it up
-      dataSetName = dataSet.data_set_name;
-      seenAlready = {
-        data_set_name: dataSet.data_set_name,
-        sample_labels: [],
-      };
-    }
-    // combine the samples together
-    seenAlready.sample_labels =
-        seenAlready.sample_labels.concat(dataSet.sample_labels)
-    dataSetHash[dataSet.data_set_id] = seenAlready;
-  });
-  var comboSampleGroupDataSets = _.map(dataSetHash,
-      function (samplesAndName, data_set_id) {
-    return {
-      data_set_id: data_set_id,
-      data_set_name: samplesAndName.data_set_name,
-      sample_labels: samplesAndName.sample_labels,
-
-      // I think we can fake this
-      unfiltered_sample_count: 1,
-    };
-  });
-
-
-
-
   var self = this;
   var deferred = Q.defer();
 
-  // define up here so as to be available throughout promise chain (so that
-  // we can skip a .then block)
+  // this path will be where the expression data is scooped up from
   var expressionDataPath = path.join(workDir, "data_expression.txt");
-  var logPath = path.join(workDir, "*.log");
-  var associated_object = {
-    collection_name: "SampleGroups",
-    mongo_id: this.job.args.sample_group_id,
-  };
-  var form_id = this.job.args.form_id;
 
   Q.all([
       // write mongo data to files
 
       // expression data to a file for use in Limma
-      spawnCommand(getSetting("cbio_genomic_expression_export"), [
+      spawnCommand(getSetting("genomic_expression_export"), [
         "--sample_group_id", this.job.args.sample_group_id,
         "--cbio",
         "--uq-sample-labels",
       ], workDir, { stdoutPath: expressionDataPath }),
       // phenotype file for cbio importer
-      spawnCommand(getSetting("clinical_export"), [
+      spawnCommand(getSetting("cbioportal_clinical_export"), [
         "--sample_group_id",
         this.job.args.sample_group_id,
         "--form_id",
         this.job.args.form_id,
         "--work-dir",
-
         workDir,
       ], workDir),
     ])
@@ -113,27 +66,29 @@ UpdateCbioData.prototype.run = function () {
         getSetting("cbio_core_jar_path"),
         "-s",
         workDir,
-      ], workDir );
+      ], workDir);
     })
     .then(Meteor.bindEnvironment(function (cbioImportResult) {
       if (cbioImportResult.exitCode !== 0) {
         throw "Problem running cbio importer";
       }
-      function printError (err) {
-         if (err) {
-           console.log("error creating blob:", err);
-         }
-       }
-      var meta = { form_id: form_id, cbio_import: dataSetName};
-      console.log('importing into cbio form', form_id, dataSetName, associated_object)
-      Blobs2.create(cbioImportResult.stdoutPath, associated_object, meta, printError);
 
-     deferred.resolve(cbioImportResult);
-   }, deferred.reject))
-    .catch(Meteor.bindEnvironment(function (reason) {
+      let associatedObj = {
+        collection_name: "Jobs",
+        mongo_id: self.job._id,
+      };
+      console.log("associatedObj:", associatedObj); // XXX
 
-      deferred.reject(reason);
-    }, deferred.reject));
+      Blobs2.create(cbioImportResult.stdoutPath, associatedObj, {}, (error) => {
+        if (error) {
+          console.log("Error creating cBioPortal stdout");
+          deferred.reject(error);
+        } else {
+          deferred.resolve();
+        }
+      });
+    }, deferred.reject))
+    .catch(deferred.reject);
   return deferred.promise;
 };
 
