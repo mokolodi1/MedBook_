487cdc4411e3eec9986f768e053d5c2d404ff8aa
Ubuntu <ubuntu@medbook-prod-2.uesh4wypts1exh5dibpq5lgfzg.dx.internal.cloudapp.net>
2016-11-21 11:04:29 -0800

    add top 5% to outlier analysis exporter
    also wasn't pulling correctly from mongo for some reason, fixed that

--- tools/old-external-tools/export_outlier_tsv/prod/get_oa.sh
+++ tools/old-external-tools/export_outlier_tsv/prod/get_oa.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 echo $1
-query="{_id:'"$1"'},{'output.up_genes':1, 'output.down_genes':1}"
+#query="{_id:'"$1"'},{'output.up_genes':1, 'output.down_genes':1}"
+query="{_id:'"$1"'}"
 #cmd='mongoexport --host mongo -d MedBook -c jobs -q "'$query'"'
 #echo $cmd
 mongoexport --host mongo -d MedBook -c jobs -q "$query" > result.json
