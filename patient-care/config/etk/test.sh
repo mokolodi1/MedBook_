53720c2efdfd71e5a1b0a9cfdc81b9a7c57d586f
ETK <e-t-k@users.noreply.github.com>
2016-11-22 12:33:13 -0800

    ACTUALLY only build patient care + local test.sh
    - d'oh, travis.yml was still building everything
    - add test.sh for running the mocha tests on local laptop

---
+++ patient-care/config/etk/test.sh
@@ -0,0 +1,5 @@
+export MONGO_URL="mongodb://localhost:27017/MedBook"
+export MEDBOOK_FILESTORE=/tmp/filestore
+export PACKAGE_DIRS=../../packages
+
+meteor --port 3001 --settings ../config/etk/settings.json test --once --driver-package dispatch:mocha
